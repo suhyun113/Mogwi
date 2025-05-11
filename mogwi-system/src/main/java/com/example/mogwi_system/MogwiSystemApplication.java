2efa057074d4b1a6d0591e8c1f3b65491de5a00e
김수현 <suhyun_113@naver.com>
2025-05-11 19:44:39 +0900

    Chore: Spring Boot 초기 세팅 및 테스트 파일 추가

---
+++ mogwi-system/src/main/java/com/example/mogwi_system/MogwiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.mogwi_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogwiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogwiSystemApplication.class, args);
+	}
+
+}
---
+++ mogwi-system/src/test/java/com/example/mogwi_system/MogwiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mogwi_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogwiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
