c86727944df64b5415a3bd4913751f2fca6abfe3
김수현 <suhyun_113@naver.com>
2025-05-19 14:05:31 +0900

    Chore (frontend): 나의 학습 페이지 닉네임 적용

--- frontend/src/views/MyStudyView.vue
+++ frontend/src/views/MyStudyView.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="mystudy">
-    <h1>나의 학습 페이지</h1>
+    <h1>{{ username }}나의 학습 페이지</h1>
     <p>로그인 후 이동한 화면입니다.</p>
   </div>
 </template>
 
 <script setup>
-// 비어있음
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const username = computed(() => store.state.store_username)
 </script>
