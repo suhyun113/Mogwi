7632c9f95fdc40e2a80ff862766e13d2fd2197fb
김수현 <suhyun_113@naver.com>
2025-06-23 01:44:23 +0900

    Feat (frontend): 네트워크 주소 추가

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
 module.exports = {
   outputDir: '../src/main/resources/static', // 빌드 타겟 디렉토리
   devServer: {
+    host: '0.0.0.0',
+    port: 8080,
     proxy: {
       '/api': {
         // '/api' 로 들어오면 포트 8000(스프링 서버)로 보낸다
